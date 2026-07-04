e97bc4ee2f91088196e6b2539885de3d7ec8e140
thee <theecares@gmail.com>
2026-07-04 23:25:09 +0500

    navbar

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -1,16 +1,14 @@
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/ui/Navbar'
 
-export default function RootLayout({
+export default function DashboardGroupLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+    </>
   )
 }
